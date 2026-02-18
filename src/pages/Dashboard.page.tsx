b2e5eb469e7c33d0398e564aff620ff689648c39
Jay Reyes <jaredreyes039@gmail.com>
2026-02-18 14:31:08 -0700

    Fixed sending creds with request from dashboard sample page

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -1,21 +1,20 @@
 import { useEffect } from "react"
-import { redirect } from '@tanstack/react-router'
+import { redirect, useNavigate } from '@tanstack/react-router'
 import axios from "axios"
 
 export default function DashboardSamplePage() {
 	const AUTH_STATUS_LINK = "http://localhost:5000/auth/status"
+	const nav = useNavigate();
 
 	// Check if User already logged in within session age
 	useEffect(() => {
 		async function fetchUserSessionStatus() {
-			const userStatus = await axios.get(AUTH_STATUS_LINK, { validateStatus: (status) => { return status < 500 } }).then((res: any) => { return res })
+			const userStatus = await axios.get(AUTH_STATUS_LINK, { validateStatus: (status) => { return status < 500 }, withCredentials: true }).then((res: any) => { return res })
 			if (userStatus.status !== 200) {
-				return;
+				nav({ to: "/" })
 			}
 			else {
-				throw redirect({
-					to: '/dashboard',
-				})
+				console.log("Success");
 			}
 		}
 		fetchUserSessionStatus();
