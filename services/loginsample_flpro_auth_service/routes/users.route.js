9c0a3b40bb291f0e2007242c102ffc5f86574fd0
Jay Reyes <jaredreyes039@gmail.com>
2026-05-22 00:17:14 +0000

    Removed unused user route

--- services/loginsample_flpro_auth_service/routes/users.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const ROUTER = express.Router();
-const USERS_CONTROLLER = require('../controllers/users.controller');
-
-// ROUTER.post('/:user_id', USERS_CONTROLLER.getUserById)
-
-exports.USERS = ROUTER;
