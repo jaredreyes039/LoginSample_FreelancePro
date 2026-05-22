dc37357d8ccc266ea2f96bbf4c8a45ba7fd621b6
Jay Reyes <jaredreyes039@gmail.com>
2026-05-22 23:34:55 +0000

    More size adjustments to logo cuz I messed them up on accidennt whoops :)

--- loginsample_flpro_ui/src/App.tsx
+++ loginsample_flpro_ui/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
 					<div className="w-full max-w-md">
 						<div className="mb-8">
 							<div className="flex items-center">
-								<img className="md:w-72 md:h-72" src={logo} />
+								<img className="w-72 h-72 md:w-96 md:h-96" src={logo} />
 							</div>
 							<>
 								<h1 className="text-gray-900 mb-2">Welcome to Client Stack</h1>
@@ -58,7 +58,7 @@ function App() {
 					<div className="w-full max-w-md">
 						<div className="mb-8">
 							<div className="flex items-center">
-								<img className="md:w-72 md:h-72" src={logo} />
+								<img className="w-72 h-72 md:w-96 md:h-96" src={logo} />
 							</div>
 
 							<>
@@ -81,7 +81,7 @@ function App() {
 					<div className="w-full max-w-md">
 						<div className="mb-8">
 							<div className="flex items-center">
-								<img className="md:w-96 md:h-96" src={logo} />
+								<img className="w-72 h-72 md:w-96 md:h-96" src={logo} />
 							</div>
 
 							<>
