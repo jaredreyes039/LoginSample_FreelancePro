a97839f68b4938daaef77dd76d93345d98b70362
Jay Reyes <jaredreyes039@gmail.com>
2026-05-25 03:24:04 +0000

    Fix logout status code check from 200 to 201

--- loginsample_flpro_ui/src/pages/Dashboard.page.tsx
+++ loginsample_flpro_ui/src/pages/Dashboard.page.tsx
@@ -18,7 +18,7 @@ export default function DashboardSamplePage() {
 				withCredentials: true,
 				validateStatus: (status) => { return status < 500 }
 			});
-			if (res.status === 201) {
+			if (res.status === 200) {
 				setIsLoading(false)
 				nav({ to: '/' })
 			}
